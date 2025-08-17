18546ce508281b62a32f5461521cd7fd23eecbd0
M-Saim-786 <saimrashid3344@gmail.com>
2025-08-17 18:56:34 +0500

    PWA done

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,21 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+// 👇 PWA Service Worker Register
+import { registerSW } from 'virtual:pwa-register'
 
-createRoot(document.getElementById("root")!).render(<App />);
+registerSW({
+    onNeedRefresh() {
+        console.log('New content available, refresh needed.')
+    },
+    onOfflineReady() {
+        console.log('App ready to work offline.')
+    },
+})
+
+
+createRoot(document.getElementById("root")!).render(<React.StrictMode>
+    <App />
+</React.StrictMode>,);
--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -11,6 +12,36 @@ export default defineConfig(({ mode }) => ({
   },
   plugins: [
     react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      workbox: {
+        clientsClaim: true,
+        skipWaiting: true
+      },
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+      manifest: {
+        name: 'Grow Track',
+        short_name: 'GrowTrack',
+        description: 'Grow Track - Manage you daily habits',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: 'standalone',
+        scope: '/',
+        start_url: '/',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    }),
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
